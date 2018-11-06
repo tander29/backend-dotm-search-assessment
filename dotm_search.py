ff66b9ff19cebd46633feb136cb2e81d850ac6b3
Travis Anderson <thetanderson@gmail.com>
2018-11-06 11:36:40 -0500

    blank slate

--- dotm_search.py
+++ dotm_search.py
@@ -4,10 +4,3 @@
 Given a directory path, this searches all files in the path for a given text string
 within the 'word/document.xml' section of a MSWord .dotm file.
 """
-
-
-def main():
-
-
-if __name__ == '__main__':
-    main()
