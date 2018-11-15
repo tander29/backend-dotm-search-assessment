3eccc73faf25a444cf4a977a42dacabf50a19e5a
Travis Anderson <thetanderson@gmail.com>
2018-11-15 10:55:18 -0500

    apparently I had my count off

--- dotm_search.py
+++ dotm_search.py
@@ -8,40 +8,33 @@ within the 'word/document.xml' section of a MSWord .dotm file.
 # Your awesome code begins here!
 import sys
 import os
-# from docx import Document
-import doctest
 import zipfile
 import argparse
 
-# parser = argparse.ArgumentParser(description='Finding out what argparse does')
-# parser.add_argument('--dir', help='I need this')
-# parser.add_argument('string', help='I need this')
-# args = parser.parse_args()
-# if args.string:
-#     print("yayy?")
-#     print(args.dir)
-#     print(args.string)
-
 
 def get_files(string_to_find, search_directory):
-    all_files = []
+    # all_files = []
     input_path = search_directory
     string_to_find = string_to_find
-    for filename in os.listdir(input_path):
-        all_files.append(filename)
-    print_files_with_string(all_files, input_path, string_to_find)
+    # for filename in os.listdir(input_path):
+    #     all_files.append(filename)
+    print_files_with_string(os.listdir(input_path), input_path, string_to_find)
 
 
 def print_files_with_string(all_files, input_path, string_to_find):
     count_files_including_string = 0
+    all_files_searched = 0
     for my_file in all_files:
-        file_location = os.path.join(input_path, my_file)
-        if zipfile.is_zipfile(file_location):
-            count_files_including_string += decode_file(
-                file_location, string_to_find)
+        extension = os.path.splitext(my_file)
+        if extension[1] == '.dotm':
+            all_files_searched += 1
+            file_location = os.path.join(input_path, my_file)
+            if zipfile.is_zipfile(file_location):
+                count_files_including_string += decode_file(
+                    file_location, string_to_find)
     print("")
     print("Search Directory Files: {} ".format(input_path))
-    print(len(all_files))
+    print(all_files_searched)
     print ("Files found with string: '{}' " .format(string_to_find))
     print count_files_including_string
 
